3606a44f26b9fe8f6063fe8c954532123a893ae7
amoswings7 <amosmakupe8@gmail.com>
2022-03-14 18:42:06 +0200

    nonsense

--- components/MySkills.js
+++ components/MySkills.js
@@ -1,47 +1,52 @@
-import htmlLogo from '../public/skills/html-logo.png'
+//import htmlLogo from '../public/skills/html-logo.png'
 import Image from 'next/image'
+import { skillsObject } from '../information/object'
+import { Spring, animated} from '@react-spring/web'
+import  {useEffect, useRef, useState} from 'react'
 
 export default function MySkills() {
+
+    const [scrollPosition, setScrollPosition] = useState(null);
+    const ele = useRef()
+
+    const scrollHandler = ()=>{
+        if(window.scrollY-ele.current.getBoundingClientRect().top >=0){
+            console.log((window.scrollY-ele.current.getBoundingClientRect().top))
+            setScrollPosition(true)
+            window.removeEventListener('scroll',scrollHandler)
+        }
+    }
+
+    useEffect(()=>{
+        window.addEventListener('scroll',scrollHandler)
+    },[])
+    
+
     return (
-        <section className='my-skills-page'>
-            <h1>MY SKILLS THAT I’M FLUENT IN</h1>
-            <main className='technologies'>
-                <div>
-                    <Image  alt='techonlogy langauage' width='150' height='150' src='/skills/html-logo.png'/>
-                    <p>HTML</p>
-                </div>
-                <div>
-                    <Image  alt='techonlogy langauage' width='150' height='150' src='/skills/css-logo.png'/>
-                    <p>CSS</p>
-                </div>
-                <div className='comment'>
-                    
-                </div>
-                <div>
-                    <Image  alt='techonlogy langauage' width='150' height='150' src='/skills/javascript-logo.png'/>
-                    <p>JAVASCRIPT</p>
-                </div>
-                <div>
-                    <Image  alt='techonlogy langauage' width='150' height='150' src='/skills/react-logo.png'/>
-                    <p>REACTJS</p>
-                </div>
-                <div>
-                    <Image  alt='techonlogy langauage' width='150' height='150' src='/skills/nextjs-logo.png'/>
-                    <p>NEXTJS</p>
-                </div>
-                <div>
-                    <Image  alt='techonlogy langauage' width='150' height='150' src='/skills/nodejs-logo.png'/>
-                    <p>NODEJS</p>
-                </div>
-                <div>
-                    <Image  alt='technology langaugge' width='150' height='150' src='/skills/mysql-logo.png'/>
-                    <p>MYSQL</p>
-                </div>
-                <div>
-                    <Image  alt='techonlogy langauage' width='150' height='150' src='/skills/mongodb-logo.png'/>
-                    <p>MONGODB</p>
-                </div>
-            </main>
+        <section ref={ele} className='my-skills-page'>
+            <h1>MY SKILLS THAT I’M FLUENT IN {scrollPosition}</h1>
+            {scrollPosition && <main className='technologies'>
+                {skillsObject.map(skill => (
+                    <Spring
+                       key={(Math.random()*10000)*(Math.random()*10000)}
+                       from={{opacity:0}}
+                       to={{opacity:1}}
+                       delay={1000}
+                       config={{duration:1000}}
+                    >
+                        {
+                            (props)=>(
+                                <animated.div style={props}>
+                                    <div>
+                                        <Image  alt={skill.alt} width={150} height={150} src={skill.src}/>
+                                        <p>{skill.name}</p>
+                                    </div>
+                                </animated.div>
+                            )
+                        }
+                    </Spring>
+                ))}
+            </main>}
         </section>
     )
 }
---
+++ information/object.js
@@ -0,0 +1,44 @@
+const skillsObject = [
+    {
+        name:'HTML',
+        src:'/skills/html-logo.png',
+        alt:'techonlogy langauage'
+    },
+    {
+        name:'Css',
+        src:'/skills/css-logo.png',
+        alt:'techonlogy langauage'
+    },
+    {
+        name:'JAVASCRIPT',
+        src:'/skills/javascript-logo.png',
+        alt:'techonlogy langauage'
+    },
+    {
+        name:'REACT',
+        src:'/skills/react-logo.png',
+        alt:'techonlogy langauage'
+    },
+    {
+        name:'NEXT.JS',
+        src:'/skills/nextjs-logo.png',
+        alt:'techonlogy langauage'
+    },
+    {
+        name:'NODE.JS',
+        src:'/skills/nodejs-logo.png',
+        alt:'technology langauage'
+    },
+    {
+        name:'MYSQL',
+        src:'/skills/mysql-logo.png',
+        alt:'techonlogy langauage'
+    },
+    {
+        name:'MONGODB',
+        src:'/skills/mongodb-logo.png',
+        alt:'techonlogy langauage'
+    }
+];
+
+export {skillsObject}
